ee39ed920d8da71e792b0abfc4f1b74cbc05a00f
Gaurav Agarwal <algogrit@gmail.com>
2020-02-21 19:33:34 +0530

    Updating send-email.sh

--- send-email.sh
+++ send-email.sh
@@ -4,8 +4,8 @@ set -e
 
 go build .
 
-./template-mailer test.json templates/invite-template.txt "Go February 2020 meetup"
+# ./template-mailer test.json templates/invite-template.txt "Go February 2020 meetup"
 
 # ./template-mailer people.json templates/invite-template.txt "Go February 2020 meetup"
 
-# ./template-mailer mails/current-rsvp.json templates/invite-template.txt "See you tomorrow at Go meetup"
+./template-mailer mails/current-rsvp.json templates/reminder-template.txt "See you tomorrow at Go meetup - Feb 2020 Edition"
