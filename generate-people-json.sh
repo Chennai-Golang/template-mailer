484578f28d2c3edbe21a2b8c818b060e83fb2413
Gaurav Agarwal <algogrit@gmail.com>
2020-02-21 19:39:47 +0530

    Updating generate script.

--- generate-people-json.sh
+++ generate-people-json.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 
-_invite_file_path="past-mails/2019/1912-invite.json"
-_reminder_file_path="past-mails/2019/191213-reminder.json"
+_invite_file_path="past-mails/2020/02-invites.json"
+_reminder_file_path="past-mails/2020/02-reminders.json"
+_existing_mails_file="mails/existing.json"
+_current_rsvp_file="mails/current-rsvp.json"
 
 # Combine invite and reminder list from previous meetups
 jq -s '[ .[0] + .[1] | group_by(.Email | ascii_downcase)[] | add ]' $_invite_file_path $_reminder_file_path | \
   jq '[ .[] | .Email |= ascii_downcase ]' | \
-  jq '[ sort_by(.Email) | .[] | .IsRSVPed = false ]' > mails/existing.json
-
-_file1_path="mails/existing.json"
-_file2_path="mails/current-rsvp.json"
+  jq '[ sort_by(.Email) | .[] | .IsRSVPed = false ]' > $_existing_mails_file
 
 # Sort files before comparing - DESTRUCTIVE!
-cat $_file1_path | jq '[ .[] | .Email |= ascii_downcase ]' | jq 'sort_by(.Email)' > "$_file1_path-sorted"
-mv "$_file1_path-sorted" $_file1_path
+cat $_existing_mails_file | jq '[ .[] | .Email |= ascii_downcase ]' | jq 'sort_by(.Email)' > "$_existing_mails_file-sorted"
+mv "$_existing_mails_file-sorted" $_existing_mails_file
 
-cat $_file2_path | jq '[ .[] | .Email |= ascii_downcase ]' | jq 'sort_by(.Email)' > "$_file2_path-sorted"
-mv "$_file2_path-sorted" $_file2_path
+cat $_current_rsvp_file | jq '[ .[] | .Email |= ascii_downcase ]' | jq 'sort_by(.Email)' > "$_current_rsvp_file-sorted"
+mv "$_current_rsvp_file-sorted" $_current_rsvp_file
 
 # Check for duplicates
-jq -r '.[] | .Email' $_file1_path > /tmp/uniq-mail-check-file1.txt
-jq -r '.[] | .Email' $_file2_path > /tmp/uniq-mail-check-file2.txt
+jq -r '.[] | .Email' $_existing_mails_file > /tmp/uniq-mail-check-file1.txt
+jq -r '.[] | .Email' $_current_rsvp_file > /tmp/uniq-mail-check-file2.txt
 
 # Duplicate emails
 echo "------Duplicate emails!------"
@@ -28,7 +27,7 @@ comm -1 -2 /tmp/uniq-mail-check-file1.txt /tmp/uniq-mail-check-file2.txt
 echo "-----------------------------"
 
 # Remove duplicates from file1!
-comm -1 -2 /tmp/uniq-mail-check-file1.txt /tmp/uniq-mail-check-file2.txt | xargs -n 1 -- ./dedup.sh $_file1_path
+comm -1 -2 /tmp/uniq-mail-check-file1.txt /tmp/uniq-mail-check-file2.txt | xargs -n 1 -- ./dedup.sh $_existing_mails_file
 
 # Combine file1 with file2 contents
-jq -s '.[0] + .[1]' $_file1_path $_file2_path | tee people.json
+jq -s '.[0] + .[1]' $_existing_mails_file $_current_rsvp_file | tee people.json
